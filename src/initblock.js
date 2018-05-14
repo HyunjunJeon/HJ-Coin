57c761e5c007fd8283afbb91b8eb613dbf3b7fdd
Jeonhyunjun <jeonhj920@gmail.com>
2018-05-14 22:33:47 +0900

    1. Validation Function     - Block Structure     - Chain     - Replacing Chain and Adding a Block to the Chain

--- src/initblock.js
+++ src/initblock.js
@@ -26,7 +26,10 @@ const getLastBlock = () => blockchain[blockchain.length -1];
 
 const getTimeStamp = () => new Date().getTime() / 1000;
 
-const createHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+const getBlockChain = () => blockchain;
+
+const createHash = (index, previousHash, timestamp, data) => // data를 string 처리해버림 -> 추후 검증때 문제가 생길 수 있으므로
+    CryptoJS.SHA256(index + previousHash + timestamp + JSON.stringify(data)).toString();
 
 const createNewBlock = data => {
     const previousBlock = getLastBlock();
@@ -35,14 +38,18 @@ const createNewBlock = data => {
     const newHash = createHash(newBlockIndex, previousBlock.hash, newTimeStamp, data);
     const newBlock = new Block(newBlockIndex, previousBlock.hash, newTimeStamp, data);
     return newBlock;
-};
+}
 
 const getBlockHash = (block) => createHash(block.index, block.previousHash, block.timestamp, block.data);
 
-// Validation - 다른사람도 블록을 추가할 수 있으므로 검증이 필수적
+// Validation - 다른사람도 블록을 추가할 수 있으므로 Validation이 필수적
 
 const isNewBlockValid = (candidateBlock, latestBlock) => {
-    if(latestBlock.index + 1 !== candidateBlock.index){
+    if(!isNewStructrueValid(candidateBlock)){
+        console.log("The candidate block is not valid")
+        return false;
+    }
+    else if(latestBlock.index + 1 !== candidateBlock.index){
         console.log("The candidate Block doesn't have a valid index");
         return false;
     }else if (latestBlock.hash !== candidateBlock.previousHash){
@@ -53,4 +60,46 @@ const isNewBlockValid = (candidateBlock, latestBlock) => {
         return false;
     }
     return true;
-};
+}
+
+const isNewStructrueValid = block => {
+    return (
+        typeof block.index === "number" &&
+        typeof block.hash === "string" &&
+        typeof block.previousHash === "string" &&
+        typeof block.timestamp === "number" &&
+        typeof block.data === "string"
+    );
+}
+
+const isChainValid = (candidateChain) => {
+    const isGenesisValid = block => JSON.stringify(block) === JSON.stringify(genesisBlock);
+    if(!isGenesisValid(candidateChain[0])){ // 후보 체인의 genesisBlock 검증
+        console.log("The candidate Chain's genesisBlock is not the smae as our genesis Block");
+        return false;
+    }
+    for(let i = 1; i < candidateChain.length; i++){ // genesis block은 이전 해쉬가 없으므로 검증에서 제외
+        if(!isNewBlockValid(candidateChain[i],candidateChain[i-1])){
+            return false;
+        }
+    }
+    return true;
+}
+
+const replaceChain = candidateChain => {
+    if(isChainValid(candidateChain) && candidateChain.length > getBlockChain().lenth){
+        blockchain = candidateChain;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const addBlockToChain = candidateBlock => {
+    if(isNewBlockValid(candidateBlock,getLastBlock())){
+        getBlockChain().push(candidateBlock);
+        return true;
+    } else{
+        return false;
+    }
+}
