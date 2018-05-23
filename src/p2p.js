f5a2a62cbbccc3a552de85cb7dc45ba95e34f71a
HyunjunJeon <jeonhj920@gmail.com>
2018-05-23 22:14:11 +0900

    Connect P2P - 1

--- src/p2p.js
+++ src/p2p.js
@@ -1,13 +1,39 @@
+/*
+  WebSocket은 HTTP 와 달리
+  Connection이 유지됌. 그래서 속도가 더 빠르고 Resource 낭비가 적음
+  페이지를 다시 보내고 요청하는 등의 과정을 거칠 필요가 없음
+  => 서버와 Real-Time Connection 인거임
+*/
+
+// socket은 peer 와 peer 사이의 대화 통로
 const WebSocket = require('ws');
 
 const sockets = [];
 
+const getSockets = () => sockets;
+
+// HTTP, P2P 서버는 같은 '포트'에 존재 가능 => Protocol이 다르니까
 const StartP2PServer = server => {
   const wsServer = new WebSocket.Server({server});
-  wsServer.on("Connection", ws => {
+  wsServer.on("Connection", ws => { 
     console.log(`Hello ${ws}`);
   });
   console.log("HJ Coin P2P Server is Running~~!!");
-}
+};
 
-module.exports = StartP2PServer
+const initSocketConnection = socket => {
+  sockets.push(socket);
+};
+
+// peer 와 peer 간 연결
+const connectToPeers = newPeer => {
+  const ws = new WebSockets(newPeer);
+  ws.on("open the connection", () => {
+    initSocketConnection(ws);
+  });
+};
+
+module.exports = {
+  StartP2PServer,
+  connectToPeers
+};
--- src/server.js
+++ src/server.js
@@ -5,33 +5,42 @@ const express = require("express"),
   P2P = require("./p2p");
 
 const {getBlockChain, createNewBlock} = Blockchain;
-const {StartP2PServer} = P2P;
+const {StartP2PServer, connectToPeers} = P2P;
 
+// 실행 시 export HTTP_PORT=4000 같은걸 넣어줄 수 있도록
 const PORT = process.env.HTTP_PORT || 3000;
 
 const app = express();
 
-// Middle Ware
+// ***Middle Ware***
 app.use(bodyParser.json());
 app.use(morgan("combined"));
 
-// Routing
-// VSC - Rest Client
-app.get("/blocks", (req,res) => { // 블록 보여주기
+// ***Routing*** ,,, VSC - Rest Client
+
+  // 블록 보여주기
+app.get("/blocks", (req,res) => { 
   res.send(getBlockChain());
 });
 
-app.post("/blocks", (req,res) => { // 블록 채굴
-  const {body:{data}} = req;
+  // 블록 채굴
+app.post("/blocks", (req,res) => { 
+  const {body : {data} } = req;
   const newBlock = createNewBlock(data);
   res.send(newBlock);
 });
 
+  // Connect Peers Each Other
+app.post("/peers", (req,res)=>{
+  const {body : {peer} } = req;
+  connectToPeers(peer);
+  res.send();
+});
 
-// Server Running
+// ***Server Running***
   // HTTP Server
 const server = app.listen(PORT, 
   () => console.log(`HJ Coin HTTP Server Running On ${PORT}`)
 );
-  // P2P Server
+  // P2P Server (need WebServer)
 StartP2PServer(server);
