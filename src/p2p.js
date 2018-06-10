f38d758ccfc1c50aca9609e0588346df450647b9
HyunjunJeon <jeonhj920@gmail.com>
2018-06-10 16:38:30 +0900

    Sync, Checking, BroadCasting Done

--- src/p2p.js
+++ src/p2p.js
@@ -44,12 +44,13 @@ const getBlockChainResponse = data => {
 const getSockets = () => sockets;
 
 const initSocketConnection = ws => {
-  sockets.push(socket);
+  sockets.push(ws);
   handleSocketMessage(ws);
   handleSocketError(ws);
   sendMessage(ws,getLastest());
 };
 
+// 데이터를 가져와서 Parse 시키는데 혹시나 Error를 위한 try-catch
 const parseData = data => {
   try{
     return JSON.parse(data);
@@ -67,16 +68,17 @@ const responseLatest = () => getBlockChainResponse([getLastBlock()]);
 
 const responseAll = () => getBlockChainResponse(getBlockChain());
 
+// Message를 얻었을 때 어떻게 해야하는가??
 const handleSocketMessage = ws => {
-  ws.on("Message",data => {
-    const message = parseData(data);
+  ws.on("Message", data => {
+    const message = parseData(data); // 데이터를 JSON 타입으로 바꾸고
     if(message == null){
       return;
     }
-    console.log(message);
-    switch(message.type){
-      case GET_LATEST:
-        sendMessage(ws,responseLatest());
+    console.log(message); // Check 용 Console 출력
+    switch(message.type){ // Message 타입별 기능 구분
+      case GET_LATEST: // 가장 최근 블록 가져오기
+        sendMessage(ws,responseLatest()); 
         break;
       case BLOCKCHAIN_RESPONSE:
         const receiveBlocks = message.data;
@@ -93,26 +95,24 @@ const handleSocketMessage = ws => {
 };
 
 const handleBlockchainResponse = receiveBlocks => {
-  if(receiveBlocks.length === 0){
-    console.log("Receive Blocks have a lenth of 0")
+  if(receiveBlocks.length === 0){ // 받은 블록이 비어있다면
+    console.log("Receive Blocks have a lenth of 0");
     return ;
   }
-
-  const latestBlockReceived = receiveBlocks[receiveBlocks.lenth -1];
-  if(!isBlockStructrueValid(latestBlockReceived)){
+  const latestBlockReceived = receiveBlocks[receiveBlocks.lenth -1]; // 가장 마지막 블록을 선택
+  if(!isBlockStructrueValid(latestBlockReceived)){ // 구조가 확실한지 확인
     console.log("The Block Structure of the Block Receive is Not Valid");
     return;
   }
-
   const newestBlock = getLastBlock();
   if(latestBlockReceived.index > newestBlock.index){
-    if(newestBlock.hash === latestBlockReceived.previousHash){ // 1개 블록 앞서있을 때
+    if(newestBlock.hash === latestBlockReceived.previousHash){ // 1개 블록 앞서있을 때 ( 바로 옆에 붙어있음 )
       if(addBlockToChain(latestBlockReceived)){
         broadCastNewBlock();
       }
-    }else if(receiveBlocks.length === 1){ // 2개 이상의 블록을 앞서 있을 때
+    }else if(receiveBlocks.length === 1){ // 2개 이상의 블록을 앞서 있을 때 ( 멀리 떨어져 있음 )
       // 전체 블록체인을 가져옴 -> 모든 Socket에게 보내달라고 함
-      sendMessageToAll(getAll())
+      sendMessageToAll(getAll());
     }else{
       replaceChain(receiveBlocks);
     }
